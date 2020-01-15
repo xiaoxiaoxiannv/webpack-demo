e5ad0c19c7c7bfdb628e11e912ab86539a17cd38
Luna <1017929760@qq.com>
2020-01-15 16:15:21 +0800

    准备提交了

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -11,5 +11,37 @@ module.exports = {
             title: 'XDML-写代码啦',
             template: 'src/assets/index.html'
         })
-    ]
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader',
+                ],
+            },
+            {
+                test: /\.styl$/,
+                loader: ['style-loader', 'css-loader', 'stylus-loader'],
+            },
+            {
+                test: /\.less$/,
+                loader: ['style-loader', 'css-loader', 'less-loader'],
+            },
+            {
+                test: /\.scss$/i,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            implementation: require('dart-sass')
+                        }
+                    },
+                    //如果文件末尾是以.scss结尾，就使用style/css/sass loader
+                ],
+            },
+        ],
+    },
 };
